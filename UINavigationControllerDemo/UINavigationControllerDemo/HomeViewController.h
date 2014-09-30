2d8bb2fcbdb9b9a079632f4827eda53d9e1701b8
egay <egfojas@voyagerinnovation.com>
2014-09-30 12:18:13 +0800

    Created UINavigationControllerDemo Project

---
+++ UINavigationControllerDemo/UINavigationControllerDemo/HomeViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UINavigationControllerDemo
+//
+//  Created by Edgar Allan Fojas on 9/30/14.
+//  Copyright (c) 2014 Edgar Allan Fojas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+
+@end
+
---
+++ UINavigationControllerDemo/UINavigationControllerDemo/HomeViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  UINavigationControllerDemo
+//
+//  Created by Edgar Allan Fojas on 9/30/14.
+//  Copyright (c) 2014 Edgar Allan Fojas. All rights reserved.
+//
+
+#import "HomeViewController.h"
+
+@interface HomeViewController ()
+
+@end
+
+@implementation HomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
