eafed1171836820e5a26b7bdd27bb7e44cbf4f92
egay <egfojas@voyagerinnovation.com>
2014-09-30 23:38:18 +0800

    Created UISplitViewController demo project

---
+++ SplitViewControllerDemo/SplitViewControllerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SplitViewControllerDemo
+//
+//  Created by Edgar Allan Fojas on 9/30/14.
+//  Copyright (c) 2014 Edgar Allan Fojas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
