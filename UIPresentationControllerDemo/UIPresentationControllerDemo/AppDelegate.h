0e6337af7d02c97ee81424c813f6fb955ae9f76f
egay <egfojas@voyagerinnovation.com>
2014-09-30 14:34:28 +0800

    Created UIPresentationControllerDemo Project

---
+++ UIPresentationControllerDemo/UIPresentationControllerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIPresentationControllerDemo
+//
+//  Created by Edgar Allan Fojas on 9/30/14.
+//  Copyright (c) 2014 Edgar Allan Fojas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIPresentationControllerDemo/UIPresentationControllerDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UIPresentationControllerDemo
+//
+//  Created by Edgar Allan Fojas on 9/30/14.
+//  Copyright (c) 2014 Edgar Allan Fojas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
