65f8e0431b14a863c5007aaa03dd5b21410ea693
egay <egfojas@voyagerinnovation.com>
2014-09-30 14:57:13 +0800

    initial implementation of PresentationController demo

--- UIPresentationControllerDemo/UIPresentationControllerDemo/HomeViewController.m
+++ UIPresentationControllerDemo/UIPresentationControllerDemo/HomeViewController.m
@@ -7,12 +7,44 @@
 //
 
 #import "HomeViewController.h"
+#import "SecondViewController.h"
 
 @interface HomeViewController ()
+
+@property (nonatomic, strong) SecondViewController *secondViewController;
 
 @end
 
 @implementation HomeViewController
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        _secondViewController = [[SecondViewController alloc] init];
+    }
+    return self;
+}
+
+- (void)loadView
+{
+    self.view = [[UIView alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    UIButton *presentButton = [[UIButton alloc] initWithFrame:CGRectMake(50, 50, 200, 30)];
+    [presentButton addTarget:self action:@selector(didTapPresentButton:) forControlEvents:UIControlEventTouchUpInside];
+
+    UIButton *presentModalButton = [[UIButton alloc] initWithFrame:CGRectMake(50, 90, 200, 30)];
+    [presentButton addTarget:self action:@selector(didTapPresentButton:) forControlEvents:UIControlEventTouchUpInside];
+    
+    [self.view addSubview:presentButton];
+    [self.view addSubview:presentModalButton];
+}
+
+- (void)didTapPresentButton:(id)sender
+{
+    [self presentViewController:self.secondViewController animated:YES completion:nil];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
---
+++ UIPresentationControllerDemo/UIPresentationControllerDemo/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  PresentedViewController.h
+//  UIPresentationControllerDemo
+//
+//  Created by Edgar Allan Fojas on 9/30/14.
+//  Copyright (c) 2014 Edgar Allan Fojas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
